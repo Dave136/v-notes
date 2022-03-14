bed38ccbf8a2a3eb66fc7628edb28382b1479284
Dave136 <davejs136@gmail.com>
2022-03-14 10:49:50 -0400

    feat: test fn

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -19,3 +19,73 @@ export const saveStorage = (key: Keys, data: object) => {
 
   localStorage.setItem(key, dataStringify);
 };
+
+if (import.meta.vitest) {
+  const { describe, expect, it, vi, afterEach } = import.meta.vitest;
+
+  type DataMock = {
+    [i: string]: string;
+  };
+  class LocalStorageMock {
+    #data: DataMock = {};
+    #length = 0;
+
+    getItem(name: string) {
+      return this.#data[name];
+    }
+
+    setItem(name: string, value: string) {
+      this.#data[name] = value;
+      this.#length = Object.keys(this.#data).length;
+    }
+
+    removeItem(name: string) {
+      delete this.#data[name];
+      this.#length = Object.keys(this.#data).length;
+    }
+
+    length() {
+      return this.#length;
+    }
+  }
+
+  describe('LocalStorage utils', () => {
+    Object.defineProperty(global, 'localStorage', {
+      value: new LocalStorageMock(),
+    });
+
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+
+    it('It should get a empty array', () => {
+      const $getStorage = vi.fn(getStorage);
+
+      expect($getStorage(Keys.Task)).toEqual([]);
+    });
+
+    it('It should get one element', () => {
+      const $saveStorage = vi.fn(saveStorage);
+      const $getStorage = vi.fn(getStorage);
+
+      const tasks = [
+        {
+          id: 1,
+          title: 'My task',
+          done: false,
+          createdAt: new Date().toDateString(),
+          updatedAt: new Date().toDateString(),
+        },
+      ];
+
+      $saveStorage(Keys.Task, tasks);
+
+      expect($getStorage(Keys.Task)).toEqual(tasks);
+
+      $saveStorage.mockClear();
+      $saveStorage.mockReset();
+      $getStorage.mockClear();
+      $getStorage.mockReset();
+    });
+  });
+}
